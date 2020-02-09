755e83a765b7c7cad606c41d099e427c5d9ec462
SCBE Anata <scbeanata@gmail.com>
2020-02-09 18:23:08 +0800

    Updates

---
+++ precache-manifest.25c9798791ca56b4161500ccd215daac.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8511c38739bd66ef56ab83c1f459ca3",
+    "url": "/mini-interface/index.html"
+  },
+  {
+    "revision": "80db811351838d52aefd",
+    "url": "/mini-interface/static/css/2.3a00fd48.chunk.css"
+  },
+  {
+    "revision": "79176074e1152c67993b",
+    "url": "/mini-interface/static/css/main.520fa218.chunk.css"
+  },
+  {
+    "revision": "80db811351838d52aefd",
+    "url": "/mini-interface/static/js/2.81ad61a9.chunk.js"
+  },
+  {
+    "revision": "b42411fd883dff4184fb41b44f703cd2",
+    "url": "/mini-interface/static/js/2.81ad61a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79176074e1152c67993b",
+    "url": "/mini-interface/static/js/main.7cd82031.chunk.js"
+  },
+  {
+    "revision": "3417f1c36d2a208fe72f",
+    "url": "/mini-interface/static/js/runtime-main.9b0200d3.js"
+  },
+  {
+    "revision": "1c07f7d988d86d5b812e8cefcc9af85c",
+    "url": "/mini-interface/static/media/aboutme-img.1c07f7d9.png"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/mini-interface/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/mini-interface/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "e23593a00b6fbe35722ee632ac7bdc5e",
+    "url": "/mini-interface/static/media/contactpage-img.e23593a0.png"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/mini-interface/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/mini-interface/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/mini-interface/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/mini-interface/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/mini-interface/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/mini-interface/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/mini-interface/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/mini-interface/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/mini-interface/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/mini-interface/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/mini-interface/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/mini-interface/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/mini-interface/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/mini-interface/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/mini-interface/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "40dacc7d457b3b7c751a3cf90e655b22",
+    "url": "/mini-interface/static/media/landingpage-img.40dacc7d.png"
+  }
+]);
